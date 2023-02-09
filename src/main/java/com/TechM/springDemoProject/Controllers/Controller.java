fde0d078d7746364a9d879bd111a01ce74da0ce2
Ruqaia Hamed <59163691+ruqaia-hamad@users.noreply.github.com>
2023-02-09 17:05:47 +0400

    adding Customer Api , Item Api , Invoice Api

--- src/main/java/com/TechM/springDemoProject/Controllers/Controller.java
+++ src/main/java/com/TechM/springDemoProject/Controllers/Controller.java
@@ -1,8 +1,14 @@
 package com.TechM.springDemoProject.Controllers;
 
 
+import com.TechM.springDemoProject.Models.Customer;
+import com.TechM.springDemoProject.Models.Invoice;
+import com.TechM.springDemoProject.Models.Item;
 import com.TechM.springDemoProject.Models.Market;
 import com.TechM.springDemoProject.Services.CustomerService;
+import com.TechM.springDemoProject.Services.InvoiceService;
+import com.TechM.springDemoProject.Services.ItemService;
+import com.TechM.springDemoProject.Services.MarketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +20,36 @@ public class Controller {
 
     @Autowired
     CustomerService customerService;
+    @Autowired
+    MarketService marketService;
+
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    InvoiceService invoiceService;
 
     //creating API
     @RequestMapping(value = "Market/getAll", method= RequestMethod.GET)
     public List<Market> getAllMarkets(){
-        List<Market> markets=new ArrayList<>();
+        List<Market> markets=marketService.getAllMarkets();
         return markets;
+    }
+
+    @RequestMapping(value = "Customer/getAll", method= RequestMethod.GET)
+    public List<Customer> getAllCustomers(){
+        List<Customer> customers=customerService.getAllCustomers();
+        return customers;
+    }
+    @RequestMapping(value = "Item/getAll", method= RequestMethod.GET)
+    public List<Item>  getAllItems(){
+        List<Item> items=itemService.getAllItems();
+        return items;
+    }
+    @RequestMapping(value = "Invoice/getAll", method= RequestMethod.GET)
+    public List<Invoice> getAllInvoices(){
+        List<Invoice> invoices=invoiceService.getAllInvoices();
+        return invoices;
     }
 
 
@@ -29,6 +59,24 @@ public class Controller {
         customerService.addCustomer();
         customerService.addCustomer();
     }
+
+    @GetMapping(value = "addMarket")
+    public void addMarket(){
+        marketService.addMarket();
+
+    }
+
+    @GetMapping(value = "addInvoice")
+        public void addInvoice(){
+        invoiceService.addInvoice();
+        invoiceService.addInvoice();
+        }
+
+        @GetMapping(value = "addItem")
+        public void addItem(){
+        itemService.addItem();
+        itemService.addItem();
+        }
 
     @GetMapping(value = "deleteById")
     public String deleteCutomerById(@RequestParam Integer id){
