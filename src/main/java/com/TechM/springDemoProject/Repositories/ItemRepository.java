03cac041b706588d15de3add24f4f7c7452f288e
Ruqaia Hamed <59163691+ruqaia-hamad@users.noreply.github.com>
2023-02-09 17:06:45 +0400

    adding method in Item Repository

--- src/main/java/com/TechM/springDemoProject/Repositories/ItemRepository.java
+++ src/main/java/com/TechM/springDemoProject/Repositories/ItemRepository.java
@@ -1,4 +1,13 @@
 package com.TechM.springDemoProject.Repositories;
+import com.TechM.springDemoProject.Models.Item;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ItemRepository {
+import java.util.List;
+
+@Repository
+public interface ItemRepository  extends CrudRepository<Item,Integer> {
+    @Query(value="SELECT m FROM Item m")
+    List<Item> getAllItems();
 }
