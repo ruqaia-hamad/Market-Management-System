269de7a562b8f4298e0d7a44b380cd3d8196e120
Ruqaia Hamed <59163691+ruqaia-hamad@users.noreply.github.com>
2023-03-12 16:23:43 +0400

    cotroller class

---
+++ src/main/java/com/TechM/springDemoProject/Controllers/EmailController.java
@@ -0,0 +1,42 @@
+package com.TechM.springDemoProject.Controllers;
+
+import com.TechM.springDemoProject.mailing.models.EmailDetails;
+import com.TechM.springDemoProject.mailing.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "Email")
+public class EmailController {
+    @Autowired
+    EmailService emailService;
+
+
+    @PostMapping("/sendMail")
+    public String sendMail(@RequestBody EmailDetails emailDetails) {
+        String status = emailService.sendSimpleMail(emailDetails);
+        return status;
+    }
+
+    @PostMapping("/sendMailToMany")
+    public String sendMailToMany(@RequestBody EmailDetails emailDetails) {
+        String status = emailService.sendSimpleMailToMany(emailDetails);
+        return status;
+    }
+
+
+    @PostMapping("/sendMailWithAttachment")
+    public String sendMailWithAttachment(@RequestBody EmailDetails emailDetails) {
+        String status = emailService.sendMailWithAttachment(emailDetails);
+        return status;
+    }
+
+
+    @PostMapping("/sendMailWithAttachmentToMany")
+    public String sendMailWithAttachmentToMany(@RequestBody EmailDetails emailDetails) {
+        String status = emailService.sendMailWithAttachmentToMany(emailDetails);
+        return status;
+    }
+}
