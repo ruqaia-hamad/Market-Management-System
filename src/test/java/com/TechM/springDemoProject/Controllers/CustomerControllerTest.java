5b9e4ae1ea48c34ffc010f1e245d59929f804425
Ruqaia Hamed <59163691+ruqaia-hamad@users.noreply.github.com>
2023-04-02 13:45:08 +0400

    test classes

--- src/test/java/com/TechM/springDemoProject/Controllers/CustomerControllerTest.java
+++ src/test/java/com/TechM/springDemoProject/Controllers/CustomerControllerTest.java
@@ -2,6 +2,7 @@ package com.TechM.springDemoProject.Controllers;
 
 import com.TechM.springDemoProject.Models.Customer;
 import com.TechM.springDemoProject.Models.Invoice;
+import com.TechM.springDemoProject.RequestObject.CustomerRequestForCreateCustomer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,17 +42,23 @@ class CustomerControllerTest {
         Customer customer = customerController.getCustomerById(2);
         assertEquals("Ahmed", customer.getCustomerFirstName());
     }
-    @Test
-    void getCustomerByIdThrowsErrorForInvalidId() throws Exception{
-        assertThrows(Exception.class, () -> customerController.getCustomerById(0));
-    }
-
 
     @Test
     void getCustomerByIdReturnsNullForNonexistentId() throws Exception {
         assertNull(customerController.getCustomerById(100));
     }
 
+    @Test
+    void getCustomerByIdReturnsNullForNonexistentId1() throws Exception {
+        assertNull(customerController.getCustomerById(80));
+    }
+    @Test
+    void getCustomerByIdReturnsNullForNonexistentId2() throws Exception {
+        assertNull(customerController.getCustomerById(79));
+    }    @Test
+    void getCustomerByIdReturnsNullForNonexistentId3() throws Exception {
+        assertNull(customerController.getCustomerById(50));
+    }
 
     @Test
     void testGetAllCustomers() throws Exception {
@@ -71,6 +78,65 @@ class CustomerControllerTest {
         Integer marketId = 2;
         String result = customerController.setIsActiveFalseByMarketId(marketId);
         assertEquals("Record updated", result);
+    }
+
+
+    @Test
+    void setIsActiveFalseByMarketId2() throws Exception {
+        Integer marketId = 5;
+        String result = customerController.setIsActiveFalseByMarketId(marketId);
+        assertEquals("Record updated", result);
+    }
+
+    @Test
+    void setIsActiveFalseByMarketId3() throws Exception {
+        Integer marketId = 4;
+        String result = customerController.setIsActiveFalseByMarketId(marketId);
+        assertEquals("Record updated", result);
+    }
+
+
+
+    @Test
+    void testDeleteAllCustomers1() throws Exception {
+        String message = customerController.deleteAllCustomers();
+        assertEquals("ALL Customer isActive=false", message);
+        List<Customer> customers = customerController.getAllCustomers();
+        for (Customer customer : customers) {
+            assertFalse(customer.getIsActive());
+        }
+    }
+
+    @Test
+    void testDeleteAllCustomers3() throws Exception {
+
+        String result = customerController.deleteAllCustomers();
+
+        assertEquals("ALL Customer isActive=false", result);
+    }
+    @Test
+    void testDeleteAllCustomers4() throws Exception {
+
+        customerController.deleteAllCustomers();
+
+        List<Customer> activeCustomers = customerController.getAllActiveCustomers();
+        assertTrue(activeCustomers.isEmpty());
+    }
+
+
+    @Test
+    void testFindTopByOrderByUpdated2() {
+        List<Customer> customers = customerController.findTopByOrderByUpdated();
+        for (Customer customer : customers) {
+            assertNotNull(customer.getUpdatedDate());
+        }
+    }
+
+    @Test
+    void testFindTopByOrderByUpdated1() {
+        List<Customer> customers = customerController.findTopByOrderByUpdated();
+        assertNotNull(customers);
+        assertFalse(customers.isEmpty());
     }
 
 
